fb94beb63957654d75da86879c4b3816cccf1e64
inoaa79 <inoaa@inoaa.space>
2022-01-29 00:28:15 +0300

    İlk Yükleme!

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.IO.Ports;
+using System.Diagnostics;
+namespace Arduino_System_Information
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Your com port, and baud rate:
+            SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            port.Open();
+            Console.WriteLine("Serial is ready!");
+            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+
+            while (true)
+            {
+                ulong used = GetUsedPhys() / (1024 * 1024);
+                ulong total = GetTotalPhys() / (1024 * 1024);
+                int islmc = (int)cpuCounter.NextValue();
+                byte[] MyMessage = System.Text.Encoding.UTF8.GetBytes($"{used}-{(int)(used * 1.0 / total * 100)}-{islmc}");
+                port.Write(MyMessage, 0, MyMessage.Length);
+
+                //Per 1000 ms
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        #region Obtain memory information API
+        [DllImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool GlobalMemoryStatusEx(ref MEMORY_INFO mi);
+
+        //Define the information structure of memory
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MEMORY_INFO
+        {
+            public uint dwLength; //Current structure size
+            public uint dwMemoryLoad; //Current memory utilization
+            public ulong ullTotalPhys; //Total physical memory size
+            public ulong ullAvailPhys; //Available physical memory size
+            public ulong ullTotalPageFile; //Total Exchange File Size
+            public ulong ullAvailPageFile; //Total Exchange File Size
+            public ulong ullTotalVirtual; //Total virtual memory size
+            public ulong ullAvailVirtual; //Available virtual memory size
+            public ulong ullAvailExtendedVirtual; //Keep this value always zero
+        }
+        #endregion
+
+     
+
+        #region Get the current memory usage
+        /// <summary>
+        /// Get the current memory usage
+        /// </summary>
+        /// <returns></returns>
+        public static MEMORY_INFO GetMemoryStatus()
+        {
+            MEMORY_INFO mi = new MEMORY_INFO();
+            mi.dwLength = (uint)System.Runtime.InteropServices.Marshal.SizeOf(mi);
+            GlobalMemoryStatusEx(ref mi);
+            return mi;
+        }
+        #endregion
+
+        #region Get the current available physical memory size
+        /// <summary>
+        /// Get the current available physical memory size
+        /// </summary>
+        /// <returns>Current available physical memory( B)</returns>
+        public static ulong GetAvailPhys()
+        {
+            MEMORY_INFO mi = GetMemoryStatus();
+            return mi.ullAvailPhys;
+        }
+        #endregion
+
+        #region Get the current memory size used
+        /// <summary>
+        /// Get the current memory size used
+        /// </summary>
+        /// <returns>Memory size used( B)</returns>
+        public static ulong GetUsedPhys()
+        {
+            MEMORY_INFO mi = GetMemoryStatus();
+            return (mi.ullTotalPhys - mi.ullAvailPhys);
+        }
+        #endregion
+
+        #region Get the current total physical memory size
+        /// <summary>
+        /// Get the current total physical memory size
+        /// </summary>
+        /// <returns&amp;gt;Total physical memory size( B)&amp;lt;/returns&amp;gt;
+        public static ulong GetTotalPhys()
+        {
+            MEMORY_INFO mi = GetMemoryStatus();
+            return mi.ullTotalPhys;
+        }
+        #endregion
+    }
+}
---
+++ README.md
@@ -0,0 +1,31 @@
+# C# Arduino System Information
+See your system information on LCD with Arduino, but with C#!
+
+You can see RAM usage, and CPU usage.
+## Requipments:
+- An Arduino Board. **[ You can use all type of Arduino types *(Leonardo, UNO, Mega etc.)* ]**
+- A 16X2 *(Minimum)* LCD Shield **with or without I2C**
+- .Net in your computer (?)
+
+**If your LCD Without I2C, you need:**
+- A 10K potentiometer 
+- A 220 Ohm resistor 
+
+
+## How to use?
+1. Download this repo.
+2. If you have I2C upload code named `systeminfoI2C.ino`, else `systeminfo.ino` to your Board.
+3. Connect pins. Look under for this. And connect your board to your computer.
+4. Write your COM port to code:
+```cs
+SerialPort port = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+```
+6. And finaly, compile. And you can see your System Info on LCD.
+Image at working:
+![image](https://user-images.githubusercontent.com/70021050/147857017-f1ea1fb9-cfdc-4139-93a1-c50da9da4bb0.jpg)
+
+Circuit of Project for **NOT I2C**:
+![tinkercad](https://user-images.githubusercontent.com/70021050/147857009-69643cf9-e338-4abb-81e5-e55f44b51196.png)
+
+**For I2C**:
+![image](https://user-images.githubusercontent.com/70021050/147857874-e9ff53cb-1a57-4f7c-a34b-258d9aaa5340.png)
